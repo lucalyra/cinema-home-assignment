e8ec68586c3f894a1906a242744fb845a7868f91
Levin <glevin@groupdomain.net>
2019-01-12 22:03:37 +0200

    Updates

---
+++ precache-manifest.fbb984646d4f434cd0cf0bbf3db3be98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b151afb4e4deae4eab42a6dec598b2d2",
+    "url": "/cinema-home-assignment/static/media/cinema_header.b151afb4.png"
+  },
+  {
+    "revision": "afd4a1a9f181ae873e04",
+    "url": "/cinema-home-assignment/static/js/runtime~main.afd4a1a9.js"
+  },
+  {
+    "revision": "a7950f802da64645cd7d",
+    "url": "/cinema-home-assignment/static/js/main.a7950f80.chunk.js"
+  },
+  {
+    "revision": "552253ba3fea2b8b7303",
+    "url": "/cinema-home-assignment/static/js/1.552253ba.chunk.js"
+  },
+  {
+    "revision": "a7950f802da64645cd7d",
+    "url": "/cinema-home-assignment/static/css/main.58f046be.chunk.css"
+  },
+  {
+    "revision": "12498b76aba376f4ea09135aa5a3a5ad",
+    "url": "/cinema-home-assignment/index.html"
+  }
+];
