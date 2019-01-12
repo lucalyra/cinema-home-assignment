6269976b7a938268899d12aa9d1d15930b8ef8fd
Levin <glevin@groupdomain.net>
2019-01-12 22:11:07 +0200

    Uploading to git pages

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -4,13 +4,13 @@ import axios from 'axios'
 class MovieService{
 
     getMovieByName(name){  
-       return axios.get('http://www.omdbapi.com/?t='+ name + '&type=movie&apikey=ebf7786a')
+       return axios.get('https://www.omdbapi.com/?t='+ name + '&type=movie&apikey=ebf7786a')
     }
     getMovieByImdbId(imdbId){
-        return axios.get('http://www.omdbapi.com/?i='+ imdbId + '&type=movie&apikey=ebf7786a')
+        return axios.get('https://www.omdbapi.com/?i='+ imdbId + '&type=movie&apikey=ebf7786a')
     }
     getMovieListByName(name){
-        return axios.get('http://www.omdbapi.com/?s='+ name + '&type=movie&apikey=ebf7786a')
+        return axios.get('https://www.omdbapi.com/?s='+ name + '&type=movie&apikey=ebf7786a')
     }
 }
 
