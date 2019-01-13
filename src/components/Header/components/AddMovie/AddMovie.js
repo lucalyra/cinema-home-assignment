1059f83a19705a11f4e3a6d66d7e7374a8f29a57
lucalyra <glevin@groupdomain.net>
2019-01-13 02:09:01 +0200

    Organizing files

--- src/components/Header/components/AddMovie/AddMovie.js
+++ src/components/Header/components/AddMovie/AddMovie.js
@@ -2,7 +2,7 @@ import './AddMovie.scss'
 import React, { Component } from 'react'
 import { connect } from "react-redux";
 import {addMovie, idGene} from '../../../../actions/appActions'
-import {noDuplicate,DuplicateAlert} from './components/Duplicate'
+import {noDuplicate,DuplicateAlert} from '../../../common/components/Duplicate'
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
